b363fe12eaf3940eabfd4791eb1f2bd81b0a4baf
minasovaash <91291526+minasovaash@users.noreply.github.com>
2021-12-24 18:08:21 +0300

    Update dz2.c

--- dz2.c
+++ dz2.c
@@ -1,41 +1,48 @@
-﻿#define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
 #include<conio.h>
 #include<stdlib.h>
-#pragma warning(disable : 4996)
+
 
 int main(void) {
 	char c;
-	int i, j, d;
+	int i, j;
 	system("chcp 1251"); 
 	system("cls");
 	do {
-		int m, n, t, k = 0, g[10][10], dem = 0, count = 0, har1 = 0;
+		int m, n, t, dem = 0, count = 0, har1 = 0;
 		printf("\nВведите количество вершин: n= "); // количество вершин
-		scanf("%d", &n);
+		scanf_s("%d", &n);
 		printf("\nВведите количество ребер: m= "); // количество ребер
-		scanf("%d", &m);
+		scanf_s("%d", &m);
 		int* x = (int*)malloc(n * sizeof(int));
-		int** h = (int**)malloc((n + 1) * sizeof(int));
-		int har[10];
-		printf("\nВводим названия вершин: ");
+		int* h = (int*)malloc((static_cast<unsigned long long>(n) + 1) * sizeof(int));
+		int** g = (int**)malloc((static_cast<unsigned long long>(n) * n) * sizeof(int));
+		int* har = (int*)malloc((static_cast<unsigned long long>(n) + 1) * sizeof(int));
+		for (int i = 0; i< n; i++) // выделяем по одной ячейке помяти размером int в каждой строке
+		{
+			g[i] = (int*)malloc(sizeof(int));
+		}
 		for (int arr = 0; arr < n; arr++) // выделяем по одной ячейке помяти размером int в каждой строке
 		{
-			h[arr] = (int*)malloc(sizeof(int));
+			h[arr] = (int)malloc(sizeof(int));
 		}
+		printf("\nВводим названия вершин: ");
+		
 		for (i = 0; i < n; i++) {
-			har[i] = 0;
 			printf("\nx[%d]=", i);
+			har[i] = 0;
+			
 			x[i] = getche(); //возвращает очередной символ, считанный с консоли, и выводит этот символ на экран.
 		}
 		printf("\nВвод рёбер: \n");
 		for (i = 0; i < m; i++)
 			for (j = 0; j < 2; j++) {
-				scanf("%d", &t);
+				scanf_s("%d", &t);
 				g[i][j] = t;
 			}
 		FILE* f;
-		f = fopen("grap.dot", "w"); // открыли файл grap.dot для записи
+		fopen_s(&f,"grap.dot", "w");
+		
 		fprintf(f, "graph mygrap{ \n");
 		for (i = 0; i < n; i++)
 			fprintf(f, "%c\n", x[i]);
@@ -44,16 +51,16 @@ int main(void) {
 		}
 		fprintf(f, "}");
 		fclose(f);
-		c = getche();
+		c = getchar();
 		system("dot -Tpng grap.dot -o grap.png");
 		for (i = 0; i < n; i++) {
 			for (j = 0; j < n; j++) {
-				if (h[i][j] == 0) har[i] = har[i] + 1;
+				if (h[i] == 0) har[i] = har[i] + 1;
 				else dem = dem + 1;
 			}
 		}
 		for (i = 0; i < n; i++) {
-			if (h[i][i] > 0 && har[i] == n - 1 || har[i] == n) {
+			if (h[i] > 0 && har[i] == n - 1 || har[i] == n) {
 				printf("Graph is not connected => Graph is not tree\n");
 				return 0;
 			}
@@ -63,12 +70,10 @@ int main(void) {
 			if ((dem / 2) == (n + 1) || count != 0) printf(" but not tree ");
 			else if (m == (n - 1)) printf("Graph is tree");
 			else  printf(" but not tree ");
-		}
-		else {
+		} else {
 			printf("Graph is not connected => Graph is not tree\n");
 		}
 		//Команды системы, чтобы передавать файл graph.dot в png и открывать картинку графа//
 	} while ((c == 'y') || (c == 'Y'));
-	getch();
 	return 0;
 }
